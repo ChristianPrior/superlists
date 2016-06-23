3824ffe349b5982517c90e2127f5433ac96079a4
Christian Prior <chris.prior.uk@gmail.com>
2016-06-23 21:38:54 +0100

    Switched pre-authenticated session to real login to fix functional tests. moved switch_to_new_window to base FT

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,21 +1,10 @@
 from .base import FunctionalTest
 from selenium.webdriver.support.ui import WebDriverWait
 import time
+
 TEST_EMAIL = 'edith@mockmyid.com'
 
 class LoginTest(FunctionalTest):
-
-	def switch_to_new_window(self, text_in_title):
-		retries = 60
-		while retries > 0:
-			for handle in self.browser.window_handles:
-				self.browser.switch_to_window(handle)
-				if text_in_title in self.browser.title:
-					return
-			retries -= 1
-			time.sleep(0.5)
-		self.fail('could not find window')
-
 
 	def test_login_with_persona(self):
 		# Edith goes to the awesome superlists site
--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,30 +1,41 @@
 from django.conf import settings
 from .base import FunctionalTest
 from .server_tools import create_session_on_server
-from .management.commands.create_session import create_pre_authenticated_session
+#from .management.commands.create_session import create_pre_authenticated_session
 import time
+
+TEST_EMAIL = 'edith@mockmyid.com'
 
 
 class MyListsTest(FunctionalTest):
 
-	def create_pre_authenticated_session(self, email):
-		if self.against_staging:
-			session_key = create_session_on_server(self.server_host, email)
-		else:
-			session_key = create_pre_authenticated_session(email)
-		## to set a cookie we need to first visit the domain
-		## 404 pages load the quickest
-		self.browser.get(self.server_url + "/404_no_such_url/")
-		self.browser.add_cookie(dict(
-			name=settings.SESSION_COOKIE_NAME,
-			value=session_key,
-			path='/',
-		))
+#	def create_pre_authenticated_session(self, email):
+#		if self.against_staging:
+#			session_key = create_session_on_server(self.server_host, email)
+#		else:
+#			session_key = create_pre_authenticated_session(email)
+#		## to set a cookie we need to first visit the domain
+#		## 404 pages load the quickest
+#		self.browser.get(self.server_url + "/404_no_such_url/")
+#		self.browser.add_cookie(dict(
+#			name=settings.SESSION_COOKIE_NAME,
+#			value=session_key,
+#			path='/',
+#		))
 
 
 	def test_logged_in_users_lists_are_saved_as_my_lists(self):
 		# Edith is a logged-in user
-		self.create_pre_authenticated_session('edith@example.com')
+#		self.create_pre_authenticated_session('edith@example.com')
+		self.browser.get(self.server_url)
+		self.browser.find_element_by_id('id_login').click()
+		self.switch_to_new_window('Mozilla Persona')
+		self.browser.find_element_by_id(
+			'authentication_email'
+		).send_keys(TEST_EMAIL)
+		self.browser.find_element_by_tag_name('button').click()
+		self.switch_to_new_window('To-Do')
+		self.wait_to_be_logged_in(email=TEST_EMAIL)
 
 		# She goes to the home page and starts a list
 		self.browser.get(self.server_url)
